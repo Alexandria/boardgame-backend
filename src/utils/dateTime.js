183c468a12a82664bb53c6794de77bf9129d6806
Alexandria <lexluka1031@gmail.com>
2019-07-02 13:14:46 -0500

    Remove unused dependencies

--- src/utils/dateTime.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.today = new Date();
-exports.date = exports.today.getFullYear() + '-' + (exports.today.getMonth() + 1) + '-' + exports.today.getDate();
-exports.time = exports.today.getHours() + ":" + exports.today.getMinutes() + ":" + exports.today.getSeconds();
-exports.curDateTime = exports.date + ' ' + exports.time;
